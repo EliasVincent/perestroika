d33df7c1b0c094fc0ddda1da95059b0a1487da9f
SoulPixelIV <william.dj@outlook.de>
2020-06-13 17:27:19 +0200

    Added random spawns

--- scenes/tests/Main.gd
+++ scenes/tests/Main.gd
@@ -3,9 +3,10 @@ extends Node2D
 var myBody = preload("res://scenes/tests/Person.tscn")
 
 func _ready():
-	var numOfSpawns = 5
+	var numOfSpawns = 500
 	for i in numOfSpawns:
 		var grabbedInstance = myBody.instance()
+		grabbedInstance.position = Vector2(rand_range(0, 800), rand_range(0, 800))
 		self.add_child(grabbedInstance)
 	
 	
