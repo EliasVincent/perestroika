469d447ee15579a13128bef9a9ff7d2972113150
SoulPixelIV <william.dj@outlook.de>
2020-06-13 21:01:57 +0200

    Worked on enemy

--- scenes/tests/PersonBehaviour.gd
+++ scenes/tests/PersonBehaviour.gd
@@ -6,9 +6,11 @@ var xSpeed = 0
 var ySpeed = 0
 
 var health = 100
+var mad = false;
+var chaseSpeed = 100
 
 enum State {IDLE, ATTACK}
-var currentState = State.ATTACK
+var currentState = State.IDLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +19,16 @@ func _ready():
 func _process(delta):
 	position += Vector2(xSpeed, ySpeed)
 	
+	#Mad Status
+	var randNum = randi()%100000+1
+	if randNum == 1:
+		mad = true
+		
+	if mad:
+		currentState = State.ATTACK
+		$Sprite.modulate = Color(1, 0, 0)
+	
+	#Change States with key
 	if Input.is_action_just_pressed("idleState"):
 		currentState = State.IDLE
 	if (Input.is_action_just_pressed("attackState")):
@@ -28,5 +40,5 @@ func _process(delta):
 			ySpeed = rand_range(-1, 1)
 		State.ATTACK:	
 			xSpeed = 0
-			ySpeed = 0		
-			position = position.move_toward(playerNode.position, delta * 100)
+			ySpeed = 0
+			position = position.move_toward(playerNode.position, delta * chaseSpeed)
