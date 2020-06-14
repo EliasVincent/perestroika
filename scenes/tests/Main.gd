9881aef9c85fd28b3647bba14bc061af0a814d56
SoulPixelIV <william.dj@outlook.de>
2020-06-14 20:29:09 +0200

    Added enemy agrorange

--- scenes/tests/Main.gd
+++ scenes/tests/Main.gd
@@ -8,7 +8,7 @@ func _ready():
 	var grabbedPlayer = myPerson.instance()
 	self.add_child(grabbedPlayer)
 	
-	var numOfSpawns = 500
+	var numOfSpawns = 1000
 	for i in numOfSpawns:
 		var grabbedInstance = myBody.instance()
 		grabbedInstance.position = Vector2(rand_range(0, 800), rand_range(0, 800))
