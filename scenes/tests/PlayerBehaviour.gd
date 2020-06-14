bdb0064e4bc3f7d07e91d7e687eaf74a276e4c54
SoulPixelIV <william.dj@outlook.de>
2020-06-14 19:39:31 +0200

    Added function to get all instances in an area

--- scenes/tests/PlayerBehaviour.gd
+++ scenes/tests/PlayerBehaviour.gd
@@ -1,17 +1,30 @@
 extends KinematicBody2D
 
+onready var enemiesFolder = get_node("/root/Main/Enemies")
+
 var velocity = Vector2(0,0)
-
 const SPEED = 200
-
 signal playerPosition
-
 var numberOfEnters = 0
 
-func getAllEnemies():
-	numberOfEnters += 1
-	print(numberOfEnters)
-
+func getAllEnemiesInRadius(distance):
+	#Get all child enemies
+	var allEnemies = enemiesFolder.get_children()
+	var selectedEnemies = []
+	#Iterate through all enemies
+	for i in allEnemies:
+		#If true add to list of selected enemies
+		if instanceInRange(i, distance):
+			var enemy = [i]
+			selectedEnemies += enemy
+	return selectedEnemies
+ 
+func instanceInRange(instance, distance):
+	#Check if instance is in range of given distance to player
+	if instance.position.x > position.x - distance and instance.position.x < position.x + distance:
+		if instance.position.y > position.y - distance and instance.position.y < position.y + distance:
+			return true
+	return false
 
 # everything physics and controls should happen in _physics_process
 func _physics_process(delta):
@@ -24,6 +37,11 @@ func _physics_process(delta):
 		velocity.x = -SPEED
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
+	
+	if Input.is_action_just_pressed("action"):
+		var enemyList = getAllEnemiesInRadius(200)
+		for i in enemyList:
+			i.queue_free()
 		
 	# moves the body, with the velocity as parameter
 	velocity = move_and_slide(velocity)
