fe7c31864486bfe2b8d0f542491419d31dcec2a1
SoulPixelIV <william.priv@outlook.de>
2020-06-15 23:42:54 +0200

    Added transition to mad status

--- scenes/tests/PersonBehaviour.gd
+++ scenes/tests/PersonBehaviour.gd
@@ -6,11 +6,12 @@ var xSpeed = 0
 var ySpeed = 0
 
 var health = 100
-var mad = false;
+var mad = false
+var madStatus = 0
+var madStatusDone = false
 var chaseSpeed = 100
 var followSpeed = 250
 var agroRange = 80
-
 var xDist = 0
 var yDist = 0
 
@@ -33,14 +34,20 @@ func _process(delta):
 	position += Vector2(xSpeed, ySpeed)
 	
 	#Mad Status
-	var randNum = randi()%100000+1
-	if randNum == 1:
+	var randNum = rand_range(0, 200000)
+	if randNum < 1:
 		mad = true
 		
-	if mad:
+	if mad and not madStatusDone:
+		madStatus += delta / 5;
+		#Change Sprite Color
+		$Sprite.modulate = Color(madStatus, 0, 0)
+		
+	if madStatus > 1:
+		madStatusDone = true;
+		madStatus = 1
 		currentState = State.ATTACK
-		$Sprite.modulate = Color(1, 0, 0)
-	
+		
 	#Change States with key
 	if Input.is_action_just_pressed("idleState"):
 		currentState = State.IDLE
