38210eff852925dc4f5f5b9dd59a728d816668bd
Elias Simon <riesyeti@outlook.de>
2020-06-18 23:38:37 +0200

    Ability Label now shows proper cost

---
+++ scenes/UI/AbilityLabel.gd
@@ -0,0 +1,7 @@
+extends RichTextLabel
+
+onready var playerNode = get_node("/root/Main/Player")
+
+func _process(delta):
+	text = ""
+	text += "Q: JOIN (" + str(playerNode.JoinCost) + ")  " + "E: CHEER (" + str(playerNode.CheerCost) + ")"
