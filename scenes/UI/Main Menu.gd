f34e2b3f35010a99f775af9983b15b3b56022ba7
Elias Vincent Simon <riesyeti@outlook.de>
2020-06-20 14:24:54 +0200

    added functioning menu

---
+++ scenes/UI/Main Menu.gd
@@ -0,0 +1,20 @@
+tool
+extends Control
+
+export var nextScene: PackedScene
+
+
+
+func _on_Play_Button_pressed():
+	get_tree().change_scene_to(nextScene)
+
+
+func _on_Jam_Link_Button_pressed():
+	OS.shell_open("https://itch.io/jam/godot-wild-jam-22")
+
+
+func _on_About_Button_pressed():
+	$"About Button/AboutDialog".popup_centered()
+
+func _on_Source_Link_Button_pressed():
+	OS.shell_open("https://github.com/EliasVincent/godot-wild-jam-22-game")
